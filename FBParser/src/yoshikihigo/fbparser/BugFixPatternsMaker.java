9922e222dfd50ee55db2679b39aeaace79cd93c2
higo <higo@192.168.3.50>
2015-10-11 20:42:26 +0200

    Three classes for creating bugfix(revisions|changes|patterns) in a given database.

---
+++ FBParser/src/yoshikihigo/fbparser/BugFixPatternsMaker.java
@@ -0,0 +1,91 @@
+package yoshikihigo.fbparser;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class BugFixPatternsMaker {
+
+	public static void main(final String[] args) {
+		FBParserConfig.initialize(args);
+		BugFixPatternsMaker main = new BugFixPatternsMaker();
+		main.make();
+	}
+
+	private void make() {
+		final String BUGFIXPATTERNS_SCHEMA = "id integer primary key, "
+				+ "beforeHash blob, " + "afterHash blob, "
+				+ "changetype integer, " + "difftype integer, "
+				+ "support integer, " + "confidence real, " + "nos integer, "
+				+ "bugfix integer";
+		final String database = FBParserConfig.getInstance().getDATABASE();
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			final Connection connector = DriverManager
+					.getConnection("jdbc:sqlite:" + database);
+
+			final Statement statement1 = connector.createStatement();
+			statement1
+					.executeUpdate("drop index if exists index_beforeHash_bugfixpatterns");
+			statement1
+					.executeUpdate("drop index if exists index_afterHash_bugfixpatterns");
+			statement1
+					.executeUpdate("drop index if exists index_beforeHash_afterHash_bugfixpatterns");
+			statement1.executeUpdate("drop table if exists bugfixpatterns");
+			statement1.executeUpdate("create table bugfixpatterns ("
+					+ BUGFIXPATTERNS_SCHEMA + ")");
+			statement1
+					.executeUpdate("create index index_beforeHash_bugfixpatterns on bugfixpatterns(beforeHash)");
+			statement1
+					.executeUpdate("create index index_afterHash_bugfixpatterns on bugfixpatterns(afterHash)");
+			statement1
+					.executeUpdate("create index index_beforeHash_afterHash_bugfixpatterns on bugfixpatterns(beforeHash, afterHash)");
+			statement1.close();
+
+			final Statement statement2 = connector.createStatement();
+			final ResultSet results2 = statement2
+					.executeQuery("select id, "
+							+ "beforeHash, "
+							+ "afterHash, "
+							+ "changetype, "
+							+ "difftype, "
+							+ "support, "
+							+ "confidence, "
+							+ "nos, "
+							+ "(select sum(bugfix) from bugfixchanges C where (C.beforeHash = P.beforeHash) and (C.afterHash = P.afterHash)) "
+							+ "from patterns P");
+			final PreparedStatement statement3 = connector
+					.prepareStatement("insert into bugfixpatterns values (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+			while (results2.next()) {
+				final int id = results2.getInt(1);
+				final byte[] beforeHash = results2.getBytes(2);
+				final byte[] afterHash = results2.getBytes(3);
+				final int changetype = results2.getInt(4);
+				final int difftype = results2.getInt(5);
+				final int support = results2.getInt(6);
+				final float confidence = results2.getFloat(7);
+				final int nos = results2.getInt(8);
+				final int bugfix = results2.getInt(9);
+				statement3.setInt(1, id);
+				statement3.setBytes(2, beforeHash);
+				statement3.setBytes(3, afterHash);
+				statement3.setInt(4, changetype);
+				statement3.setInt(5, difftype);
+				statement3.setInt(6, support);
+				statement3.setFloat(7, confidence);
+				statement3.setInt(8, nos);
+				statement3.setInt(9, bugfix);
+				statement3.executeUpdate();
+			}
+			statement2.close();
+			statement3.close();
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ FBParser/src/yoshikihigo/fbparser/BugFixRevisionsMaker.java
@@ -0,0 +1,116 @@
+package yoshikihigo.fbparser;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.SortedSet;
+import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class BugFixRevisionsMaker {
+
+	public static void main(final String[] args) {
+		FBParserConfig.initialize(args);
+		final BugFixRevisionsMaker main = new BugFixRevisionsMaker();
+		main.make();
+	}
+
+	private void make() {
+
+		final String BUGFIXREVISIONS_SCHEMA = "software string, "
+				+ "number integer, " + "date string, " + "message string, "
+				+ "bugfix integer, " + "primary key(software, number)";
+		final String database = FBParserConfig.getInstance().getDATABASE();
+		final SortedSet<Integer> bugIDs = this.getBugIDs();
+
+		try {
+			Class.forName("org.sqlite.JDBC");
+			final Connection connector = DriverManager
+					.getConnection("jdbc:sqlite:" + database);
+
+			final Statement statement1 = connector.createStatement();
+			statement1
+					.executeUpdate("drop index if exists index_number_bugfixrevisions");
+			statement1.executeUpdate("drop table if exists bugfixrevisions");
+			statement1.executeUpdate("create table bugfixrevisions ("
+					+ BUGFIXREVISIONS_SCHEMA + ")");
+			statement1
+					.executeUpdate("create index index_number_bugfixrevisions on bugfixrevisions(number)");
+			statement1.close();
+
+			final Statement statement2 = connector.createStatement();
+			final ResultSet results2 = statement2
+					.executeQuery("select software, number, date, message from revisions");
+			final PreparedStatement statement3 = connector
+					.prepareStatement("insert into bugfixrevisions values (?, ?, ?, ?, ?)");
+			while (results2.next()) {
+				final String software = results2.getString(1);
+				final int number = results2.getInt(2);
+				final String date = results2.getString(3);
+				final String message = results2.getString(4);
+				int bugfix = 0;
+				final SortedSet<Integer> relatedIDs = this.extractIDs(message);
+				for (final Integer id : relatedIDs) {
+					if (bugIDs.contains(id)) {
+						bugfix++;
+					}
+				}
+				statement3.setString(1, software);
+				statement3.setInt(2, number);
+				statement3.setString(3, date);
+				statement3.setString(4, message);
+				statement3.setInt(5, bugfix);
+				statement3.executeUpdate();
+			}
+			statement2.close();
+			statement3.close();
+
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private SortedSet<Integer> getBugIDs() {
+		final String bugFile = FBParserConfig.getInstance().getBUG();
+		final SortedSet<Integer> ids = new TreeSet<>();
+
+		try (final BufferedReader reader = new BufferedReader(
+				new InputStreamReader(new FileInputStream(bugFile),
+						"JISAutoDetect"))) {
+			reader.readLine();
+			while (true) {
+				final String lineText = reader.readLine();
+				if (null == lineText) {
+					break;
+				}
+				final Integer id = Integer.parseInt(lineText);
+				ids.add(id);
+			}
+		} catch (final IOException e) {
+			e.printStackTrace();
+		}
+
+		return ids;
+	}
+
+	private SortedSet<Integer> extractIDs(final String text) {
+		final SortedSet<Integer> numbers = new TreeSet<>();
+		final Matcher matcher = Pattern.compile("[0-9]{3,5}").matcher(text);
+		while (matcher.find()) {
+			final int startIndex = matcher.start();
+			final int endIndex = matcher.end();
+			final String numberText = text.substring(startIndex, endIndex);
+			final Integer number = Integer.parseInt(numberText);
+			numbers.add(number);
+		}
+		return numbers;
+	}
+}
