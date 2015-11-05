04b87adefe66789b25d002906f006d46e4547690
higo <higo@192.168.100.5>
2015-11-06 06:47:59 +0900

    A class for batch processing was introduced.

---
+++ FBParser/src/yoshikihigo/fbparser/BugFixDBMaker.java
@@ -0,0 +1,11 @@
+package yoshikihigo.fbparser;
+
+public class BugFixDBMaker {
+
+	public static void main(final String[] args) {
+		BugFixRevisionsMaker.main(args);
+		BugFixChangesMaker.main(args);
+		BugFixChangesUpdater.main(args);
+		BugFixPatternsMaker.main(args);
+	}
+}
