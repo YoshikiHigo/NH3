d80b4ba5479bd5889edebe5f711ca0431e0c4fe5
Yoshiki Higo <yoshiki.higo@gmail.com>
2019-09-23 18:12:12 +0900

    refactoring (remove dead code)

--- src/nh3/ammonia/Location_ADDITION.java
+++
@@ -1,9 +0,0 @@
-package nh3.ammonia;
-
-public class Location_ADDITION extends Location {
-
-  public Location_ADDITION(final String path, final int startLine, final int endLine,
-      final boolean bugfix) {
-    super(path, startLine, endLine, bugfix);
-  }
-}
--- src/nh3/ammonia/Location_UNKNOWN.java
+++
@@ -1,9 +0,0 @@
-package nh3.ammonia;
-
-public class Location_UNKNOWN extends Location {
-
-  public Location_UNKNOWN(final String path, final int startLine, final int endLine,
-      final boolean bugfix) {
-    super(path, startLine, endLine, bugfix);
-  }
-}
